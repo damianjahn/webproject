58d126d6b2733d5cd8b06ee2d7374567d0aa0fca
Damian <damian.jahn@icloud.com>
2020-08-04 17:36:46 +0200

    geting basics

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import web
 
+urls = (
+    '/(.*)/(.*)', 'index'
+)
+
+render = web.template.render("recources/")
+app = web.application(urls, globals())
+
+
+class index:
+    def GET(self, name, age):
+        return render.main(name, age)
+
+
+if __name__ == "__main__":
+    app.run()
